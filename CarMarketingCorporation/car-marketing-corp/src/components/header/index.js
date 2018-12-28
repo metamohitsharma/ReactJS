5a22a0bd60bd5cd3499f9005732c212c900db5fa
metamohitsharma <mohit.sharma@metacube.com>
2018-12-28 18:29:34 +0530

    Updated Css

--- CarMarketingCorporation/car-marketing-corp/src/components/header/index.js
+++ CarMarketingCorporation/car-marketing-corp/src/components/header/index.js
@@ -1,11 +1,12 @@
 import Brand from './brand';
+import './index.css'
 import Logo from './logo';
 import React from 'react';
 
 export default class Header extends React.Component {
     render() {
         return (
-            <div>
+            <div className="header">
                 <div>
                     <Logo />
                 </div>
