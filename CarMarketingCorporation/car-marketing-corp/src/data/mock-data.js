2fd9cf930de4a49beca13b73beeed6bf3127327e
metamohitsharma <mohit.sharma@metacube.com>
2018-12-28 12:47:16 +0530

    Updated Directory Structure

--- CarMarketingCorporation/car-marketing-corp/src/data/mock-data.js
+++ CarMarketingCorporation/car-marketing-corp/src/data/mock-data.js
@@ -1,30 +1,78 @@
-
-function brand(id, brandName) {
-    this.id = id
-    this.brandName = brandName
-}
 const brandList = [
-    new brand(1, "Hyundai"),
-    new brand(2, "Toyota"),
-    new brand(3, "Mercedes-Benz")
+    {
+        id: 1,
+        name: "Hyundai"
+    },
+    {
+        id: 2,
+        name: "Toyota"
+    },
+    {
+        id: 3,
+        name: "Mercedes-Benz"
+    }
 ]
 
-const sortBy = ["Title", "Description", "Launch Date"]
-
-function car(id, brandId, image, title, desciption, launchDate) {
-    this.id = id
-    this.brandId = brandId
-    this.image = image
-    this.title = title
-    this.desciption = desciption
-    this.launchDate = launchDate
-}
+const sortBy = [
+    {
+        id: 1,
+        name: "Title"
+    }, {
+        id: 2,
+        name: "Description"
+    }, {
+        id: 3,
+        name: "Launch Date"
+    }
+]
 
 const carList = [
-    new car(1, 1, "", "I10", "HatchBack", new Date(2016, 11, 11)),
-    new car(2, 1, "", "Elantra", "Sedan", new Date(2017, 12, 12)),
-    new car(3, 2, "", "Etios", "HatchBack", new Date(2016, 10, 1)),
-    new car(4, 2, "", "Innova", "SUV", new Date(2014, 1, 1)),
-    new car(5, 3, "", "C-Class", "Sedan", new Date(2018, 5, 1)),
-    new car(6, 3, "", "G-Class", "SUV", new Date(2018, 2, 1)),
+    {
+        id: 1,
+        brandId: 1,
+        img: "",
+        title: "I10",
+        description: "HatchBack",
+        launchDate: new Date(2016, 11, 11)
+    },
+    {
+        id: 2,
+        brandId: 1,
+        img: "",
+        title: "Elantra",
+        description: "Sedan",
+        launchDate: new Date(2017, 12, 12)
+    },
+    {
+        id: 3,
+        brandId: 2,
+        img: "",
+        title: "Etios",
+        description: "HatchBack",
+        launchDate: new Date(2016, 10, 1)
+    },
+    {
+        id: 4,
+        brandId: 2,
+        img: "",
+        title: "Innova",
+        description: "SUV",
+        launchDate: new Date(2014, 1, 1)
+    },
+    {
+        id: 5,
+        brandId: 3,
+        img: "",
+        title: "C-Class",
+        description: "Sedan",
+        launchDate: new Date(2018, 5, 1)
+    },
+    {
+        id: 6,
+        brandId: 3,
+        img: "",
+        title: "G-Class",
+        description: "SUV",
+        launchDate: new Date(2018, 2, 1)
+    }
 ]
