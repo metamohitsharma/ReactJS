1dfd36aff0203e11a5dc8b7f024935803e2aa9db
metamohitsharma <mohit.sharma@metacube.com>
2019-01-18 12:55:12 +0530

    Deleted Fiel

--- store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux'
-import rotateReducer from './reducers/rotate-reducer'
-
-export default function configureState(state = { rotating: true }) {
-    return createStore(rotateReducer, state);
-}
